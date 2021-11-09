d3d1f2d92fb698800e5fded91fbc21412eb79d66
zeph <zeph@ubook.lan>
2021-11-10 00:33:10 +0100

    cleanup

--- func.sh
+++ func.sh
@@ -4,26 +4,13 @@
 ### and color bash scheme 
 # used as .bash_profile 
 
-alias x='ssh-add -D; rm -f $SSH_AUTH_SOCK && exit'
-_DIE () { echo $1; exit 1; }
-_MYECHO () {
-  _DOTNUM=40
-  if [ -z "$1" ]; then echo "<!> need argument"; return 1; fi
-  _CHAINL=$(echo $@ | wc -c)
-  _DOTL=$((_DOTNUM - _CHAINL))
-  echo -e "$@\c"
-  i=0
-  while [ "$i" -lt "$_DOTL" ]; do
-    echo -e ".\c"
-    i=$((i+1))
-  done
-  return 0
-}
 
-                                                           ### colors
+                                                           
+### colors
 _REZ="\e[39m"
 _RDX="\e[1;31m"
-_BLX="\e[34m"                                              _GRX="\e[32m"
+_BLX="\e[34m"                                              
+_GRX="\e[32m"
 _MVX="\e[95m"
 _BLU () { echo -e "${_BLX}${@}${_REZ}" ; }
 _RED () { echo -e "${_RDX}${@}${_REZ}" ; }
@@ -32,17 +19,22 @@ _MAV () { echo -e "${_MVX}${@}${_REZ}" ; }
 _OK () { echo -e "[${_GRX}OK${_REZ}${@}]" ; }
 _KO () { echo -e "[${_RDX}KO${_REZ}${@}]" ; }
 
+
+
+
+### Functions
+
+_DIE () { echo $1; exit 1; }
+
+# ssh-agent loader
 _MAV "##################################"
 df|grep emul|awk '{print "LocalStorage = #Total="$2" #Free="$4" #Used="$3}'
 echo "HomeDir = $HOME"
 echo
-export SSH_AUTH_SOCK="/data/data/com.termux/files/home/.ssh/zeph.agent.sock"
-#rm -f $SSH_AUTH_SOCK
-#eval $(ssh-agent)
-
+export SSH_AUTH_SOCK="~/.ssh/ssh-agent.sock"
 
 _CHKSSH () {
-#set -x
+#load agent and load ~/.ssh/mykey
 _SSHAG () { ssh-add -l 2>/dev/null >/dev/null ; _RES=$? ; }
 _SSHAG
 while [ "$(echo $_RES)" -ge 1 ]; do
@@ -64,7 +56,7 @@ while [ "$(echo $_RES)" -ge 1 ]; do
     _BLU "> Add K2 ssh key ? [Y/n]"
     read -s -n1
     if [[ "$REPLY" =~ [Yy] ]]; then
-      ssh-add -q ~/.ssh/k2
+      ssh-add -q ~/.ssh/mykey
       _SSHAG
     else 
       _MYECHO "### Loaded SSH keys " && _KO
@@ -86,3 +78,38 @@ echo
 echo
 }
 _CHKSSH
+
+# alias for remove key
+alias x='ssh-add -D; rm -f $SSH_AUTH_SOCK && exit'
+
+
+
+# change PS1 var if root (used toilet^^)
+if [ $(id -u) -eq 0 ];
+then
+    PS1='\[\033[1;30m\][\[\033[1;32m\]\u\[\033[1;30m\]@\[\033[0;34m\]\h\[\033[1;30m\]] \[\033[0;36m\]\w \[\033[1;30m\]#\[\033[0m\]'
+    toilet -f smslant --metal "# root user #" && echo
+else
+    PS1='\[\033[1;30m\][\[\033[1;34m\]\u\[\033[1;30m\]@\[\033[0;35m\]\h\[\033[1;30m\]] \[\033[0;36m\]\w \[\033[1;30m\]$\[\033[0m\]'
+    toilet -f smslant --gay "Ubook"
+fi
+
+
+
+# Myecho BV@R
+
+_MYECHO () {
+  _DOTNUM=40
+  if [ -z "$1" ]; then echo "<!> need argument"; return 1; fi
+  _CHAINL=$(echo $@ | wc -c)
+  _DOTL=$((_DOTNUM - _CHAINL))
+  echo -e "$@\c"
+  i=0
+  while [ "$i" -lt "$_DOTL" ]; do
+    echo -e ".\c"
+    i=$((i+1))
+  done
+  return 0
+}
+
+
