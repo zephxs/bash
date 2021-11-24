e24c7b26510608d83a5182730c833cfe80493323
zeph <zeph@ubook.lan>
2021-11-24 21:40:43 +0100

    color and killer mod

--- profile.d/01-color.sh
+++ profile.d/01-color.sh
@@ -1,9 +1,9 @@
 ### colors
-_REZ="\e[39m"
+_REZ="\e[0m"
 _RDX="\e[1;31m"
-_BLX="\e[34m"
-_GRX="\e[32m"
-_MVX="\e[95m"
+_BLX="\e[1;34m"
+_GRX="\e[1;32m"
+_MVX="\e[1;95m"
 _BLU () { echo -e "${_BLX}${@}${_REZ}" ; }
 _RED () { echo -e "${_RDX}${@}${_REZ}" ; }
 _GRN () { echo -e "${_GRX}${@}${_REZ}" ; }
