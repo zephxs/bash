6f458e5a006eb35dc080b6540eba7c4cf0524248
note9 <dont@mail.me>
2022-06-11 17:27:41 +0200

    ssh agent multi - output clean

--- functions/sshagent-multiloader.sh
+++ functions/sshagent-multiloader.sh
@@ -15,7 +15,7 @@ while [ "$_RES" -ge 1 ]; do
   2)
     ssh-agent -a $SSH_AUTH_SOCK >$HOME/.ssh/.ssh-agent
     echo
-    _MYECHO "### Test SSH agent "
+    _MYECHO "Test SSH agent "
     sleep 1 && _SSHAG
     if [ "$_RES" = 2 ]; then
       _KO .NotLoaded && rm -f $SSH_AUTH_SOCK
@@ -24,7 +24,7 @@ while [ "$_RES" -ge 1 ]; do
     fi
     ;;
   1)
-    _MYECHO "### Check Keys "
+    _MYECHO "Check Keys "
     _KO .none
     _BLU "> Add default ssh key ? [Y/n]"
     read -s -n1
@@ -32,7 +32,7 @@ while [ "$_RES" -ge 1 ]; do
       [ -z "$_TIME" ] && ssh-add -q ${_MYSKEY} || ssh-add -t $_TIME ${_MYSKEY}
       _SSHAG
     else
-      _MYECHO "### Loaded SSH keys " && _KO
+      _MYECHO "Loaded SSH keys " && _KO
       echo && return 0
     fi
     ;;
@@ -43,11 +43,11 @@ while [ "$_RES" -ge 1 ]; do
   esac
 done
 echo
-_BLU "####################################################"
-_MYECHO "### Test SSH Agent "
+_BLU "############### SSH Agent MultiLoader ##############"
+_MYECHO "Test SSH Agent "
 [ $_RES = 0 ] && _OK .Running || _KO
 _KEY=$(ssh-add -l|awk -F/ '{print $NF}'|awk '{print $1}')
-_MYECHO "### Loaded SSH keys " && _OK .${_KEY}
+_MYECHO "Loaded SSH keys " && _OK .${_KEY}
 echo
 echo
 }
@@ -61,7 +61,7 @@ sshagent-kill () {
 _SSHPID () { awk -F'=|;' '/SSH_AGENT_PID/ {print $2}' <$HOME/.ssh/.ssh-agent ; }
 _BLU "####################################################"
 _BLU "### [dont] Kill the ssh-agent !"
-_MYECHO "### Find Agent pid "
+_MYECHO "Find Agent pid "
 if [ -z "$SSH_AGENT_PID" ]; then
   if [ -z "$(_SSHPID)" ]; then
         _KO ".NotFound"
@@ -76,12 +76,12 @@ else
   if ps aux |grep -v grep|grep -q $SSH_AGENT_PID; then _OK ".pid=$SSH_AGENT_PID"; else _KO ".NotRunning"; fi
 fi
 
-_MYECHO "### Remove Key "
+_MYECHO "Remove Key "
 if ssh-add -D &>/dev/null; then _OK; else _KO ".NoKey"; fi
-_MYECHO "### Kill Agent "
+_MYECHO "Kill Agent "
 if ssh-agent -k &>/dev/null; then _OK; else _KO; fi
 if [ -e ${SSH_AUTH_SOCK} ]; then
-_MYECHO "### Remove Socket "
+_MYECHO "Remove Socket "
   rm -f $SSH_AUTH_SOCK &>/dev/null && _OK
 fi
 if ps --user $(id -u) -F|grep -v grep|grep -q ssh-agent; then
@@ -103,7 +103,7 @@ for i in $(ps --no-header --user $(id -u) -F|grep -v grep|grep ssh-agent|awk '{p
 done
 fi
 #this one is just for style ;)
-_MYECHO "### Agent Cleanup " && _OK ".Done"
+_MYECHO "Agent Cleanup " && _OK ".Done"
 echo
 }
 
