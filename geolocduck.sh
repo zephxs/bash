88ad202a93ab830e537fe46680cabc0154b64a75
note9 <dont@mail.me>
2022-06-12 16:12:45 +0200

    geolocduck - 1.1 moved from awk to while loop and genequal fn

--- geolocduck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-### 1.0 show current Wan IP + Geolocation from Duckduckgo
-. /etc/profile.d/01-myecho-colors.sh
-_BLU "######### IP DuckDuck Geoloc #########"
-curl -s "https://duckduckgo.com/?q=ip+address" -o .tmpgeoloc
-awk -F'Your IP address is |</a>","AnswerType":"ip",' '{print $2}' .tmpgeoloc |sed -r 's/<a href.*>//; s/\)//; s/\(//; s/,//g' |awk '{print "# IP        = "$1"\n# City      = "$3"\n# ZipCode   = "$6"\n# Province  = "$4"\n# Coutry    = "$5}'
