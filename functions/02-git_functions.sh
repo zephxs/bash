a229360a4a7cafed1a8c3c04dd0cee293abe952b
zephxs <zephxs@gmail.com>
2022-10-11 22:46:39 +0200

    02-git_functions.sh - cosmetic

--- functions/02-git_functions.sh
+++ functions/02-git_functions.sh
@@ -1,7 +1,6 @@
 #############################################################
 ### Git functions
 
-
 _REPOROOTFIND () {
 ### 1.1 - function to search for all repositories to sync
 # will create ~/.reporoot file to base future sync on
@@ -18,12 +17,11 @@ if [ ! -f "$HOME/.reporoot" ]; then
 fi
 }
 
-
-pullup (){
+pullup () {
 ### 1.2 - pull all repos and all branches at once
 [ -f "$HOME/.reporoot" ] && _REPOROOT=$(cat $HOME/.reporoot) || _REPOROOTFIND
 myecho -l
-myecho -t "Git Pull all rep UP"
+myecho -t "Git - Pull all Repos"
 echo
 for _REP in $_REPOROOT; do
 myecho -l
@@ -38,8 +36,7 @@ echo
 done
 }
 
-
-repsync(){
+repsync () {
 ### 2.1 - sync all repos to a backup repo
 # here 'rep' is the backup repo (_DESTREPO), and all others repositories will be backed up in its own subfolder 
 # rsync is needed to delete files that does not exist anymore
@@ -47,7 +44,7 @@ repsync(){
 _DESTREPO=$(grep -w rep$ "$HOME/.reporoot")    # My Sync Repo name is 'rep' in this case
 _SYNCREPOS=$(cat $HOME/.reporoot | grep -v $_DESTREPO)
 myecho -l
-myecho -t "Git Sync all rep UP"
+myecho -t "Git - Repos Sync"
 [ -z "$_DESTREPO" ] && echo "Destination Repository not set.. exiting!" && return 1
 [ -z "$_SYNCREPOS" ] && echo "Source Repository not set.. exiting!" && return 1
 for _DIR in $_SYNCREPOS; do
@@ -60,19 +57,18 @@ git add .
 if [ -z "_ORIGREP" ]; then
 git commit -m "#Sync=$(date +"%H:%M-%d.%m.%Y")"
 else
-git commit -m "#Repo=$_ORIGREP #Sync=$(date +"%H:%M-%d.%m.%Y") - $_COMMITMSG"
+git commit -m "#Repo=$_ORIGREP #Sync=$(date +"%H:%M-%d.%m.%Y") #Msg=$_COMMITMSG"
 fi
 git push
 }
 
-
-gitp (){ 
+gitp () {
 ### 1.5 - add push and sync to back repo with : gitp "my comment"
 _COMMITMSG=$@
 [ -z "$_COMMITMSG" ] && { echo "Commit message missing" && return 1; }
 _ORIGREP=$(git remote get-url origin --push |awk -F'/' '{print $NF}' |uniq |sed 's/.git//')
 myecho -l
-myecho -t "Git Commit and Sync"
+myecho -t "Git - Commit and Sync"
 myecho -p "# Repo= $_ORIGREP  # Comment= $_COMMITMSG"
 git add .; git commit -m "$_COMMITMSG"; git push
 repsync
