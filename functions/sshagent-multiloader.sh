630d238234aaa3c07b755cbf2048e5d287130e99
zephxs <zephxs@gmail.com>
2024-10-20 23:03:14 +0200

    sshagent-multiloader.sh - multi key loader

--- functions/sshagent-multiloader.sh
+++ functions/sshagent-multiloader.sh
@@ -1,69 +1,108 @@
 #!/bin/bash
 
 sshagent-loader () {
-### v1.6 - Local ssh multi session agent 
+### v2.0 - Local ssh multi session agent 
+# added scan .ssh folder and load multiple keys at once
 # set fixed agent socket and default key to load
 # set key life time / empty for not setting lifetime
 # handle forwarded agent
-local _MYSKEY="$HOME/.ssh/k2"
+#local _MYSKEY="$HOME/.ssh/k2"
+local _SETKEY=""
 local _TIME="28800"
-local _VERS='v1.6'
+local _VERS='v2.0'
+unset _SSHKARRAY
+unset _LOADEDKEYS
+
 _MYECHO -t "SSH Agent MultiSession ### $_VERS"
 
+### FUNCTIONS
 # function to get agent status from ssh-add exit code
-_SSHAG (){ ssh-add -l >/dev/null 2>&1; _LOADRESULT=$?; }
+_SSHAGENTCHECK (){ ssh-add -l >/dev/null 2>&1; _LOADRESULT=$?; }
 _EXPORTAGENT (){ export SSH_AUTH_SOCK="$HOME/.ssh/ssh-agent.sock"; }
 
+_KEYLOADER(){
+# Scan .ssh folder, check if file is ssh key, and set array with choosen keys
+[ -z "${_SETKEY}" ] || { _SSHKARRAY+=($_SETKEY) && return 0; }
+_LOADEDKEYS=$(ssh-add -l |awk '{print $3}')
+_MYECHO "Scan '$HOME/.ssh/'" && _OK
+for _SSHKEY in $(find $HOME/.ssh/ -maxdepth 1 -type f |grep -Ev '.pub|ssh-agent|known|config|env|authorized'); do 
+  echo "${_LOADEDKEYS}" | grep -q "$_SSHKEY" && continue
+  ssh-keygen -l -f $_SSHKEY >/dev/null 2>&1 || continue
+  _MYECHO "Add Key= $_SSHKEY ? [Y/n]"
+  read -s -n 1
+  [ "$REPLY" = 'n' ] && _KO && continue
+  _SSHKARRAY+=($_SSHKEY)
+  _OK
+done
+_MYECHO -c "purple" -p "Keys to load: ${_SSHKARRAY[*]}"
+}
+
+_PRELOADER(){
+# set default socket to use
+  [ -e "$SSH_AUTH_SOCK" ] && rm -f "$SSH_AUTH_SOCK"
+  ssh-agent -a "$SSH_AUTH_SOCK" >$HOME/.ssh/.ssh-agent
+  _MYECHO -e "Test SSH agent"
+  sleep 0.3 && _SSHAGENTCHECK
+  if [ "$_LOADRESULT" = 2 ]; then
+    _KO ':NotLoaded' && rm -f "$SSH_AUTH_SOCK"
+    return 1
+  elif [ "$_LOADRESULT" = 1 ]; then
+    _OK ':Starting' && _SSHAGENTCHECK
+  fi
+}
+
+_AGENTLOADER(){
+# load keys to agent
+for _MYSKEY in "${_SSHKARRAY[@]}"; do
+  _MYECHO -p "Loading Key= '$_MYSKEY'"
+  [ -z "$_TIME" ] && ssh-add -q "${_MYSKEY}" || ssh-add -q -t "$_TIME" "${_MYSKEY}"
+done
+_SSHAGENTCHECK
+}
+
+### Base checks and setup
 # chmod .ssh if not already user only readable, as we will store our agent here
 [ -d "$HOME/.ssh" ] || mkdir -p $HOME/.ssh
 [ "$(stat -c '%a %n' $HOME/.ssh)" = 700 ] || chmod 700 $HOME/.ssh
 
 # test agent and setup
 [ -z "$SSH_AUTH_SOCK" ] && _EXPORTAGENT
-_SSHAG
+_SSHAGENTCHECK
 
+### MAIN
 # Act accordingly to 'ssh-add' exit code
 while [ "$_LOADRESULT" -ge 1 ]; do
   case $_LOADRESULT in
   2) # Agent not loaded
     _EXPORTAGENT
-    [ -e "$SSH_AUTH_SOCK" ] && rm -f "$SSH_AUTH_SOCK"
-    ssh-agent -a "$SSH_AUTH_SOCK" >$HOME/.ssh/.ssh-agent
-    _MYECHO -e "Test SSH agent"
-    sleep 0.3 && _SSHAG
-    if [ "$_LOADRESULT" = 2 ]; then
-      _KO ':NotLoaded' && rm -f "$SSH_AUTH_SOCK"
-      return 1
-    elif [ "$_LOADRESULT" = 1 ]; then
-      _OK ':Starting' && _SSHAG
-    fi
+    _PRELOADER
     ;;
   1) # Agent is loaded
-    _MYECHO -p "Add Key: '$_MYSKEY' ? [Y/n]"
-    read -s -n1
-    if [[ "$REPLY" =~ [Yy] ]]; then
-      [ -z "$_TIME" ] && ssh-add -q "${_MYSKEY}" || ssh-add -q -t "$_TIME" "${_MYSKEY}"
-      _SSHAG
-    else
-      _MYECHO -e "Loaded SSH keys" && _KO ':None'
-      return 0
-    fi
+    _KEYLOADER
+    _AGENTLOADER
     ;;
   *) # exit on any other output code
-    _RED "### Unknown return code.. exit"
+    _MYECHO -c red -p "# Unknown return code.. exit"
     return 1
     ;;
   esac
 done
 
 # if reached, Agent is loaded with a key
-_MYECHO -e "Test SSH Agent"
+_MYECHO "Test SSH Agent"
 [ "$_LOADRESULT" = 0 ] && _OK ':Running' || _KO
-local _KEY=$(ssh-add -l |awk -F'/| ' '{print $(NF-1)}')
-_MYECHO -e "Loaded SSH keys" && _OK ":${_KEY}"
+_LOADEDKEYS=$(ssh-add -l |awk '{print $3}' |tr "\n" " ")
+_MYECHO "Loaded SSH keys" && _OK ":${_LOADEDKEYS}"
+_MYECHO -p "Load new Key ? [yN]"
+read -s -n1
+if [[ "$REPLY" =~ [Yy] ]]; then
+  _KEYLOADER
+  _AGENTLOADER
+  _LOADEDKEYS=$(ssh-add -l |awk '{print $3}' |tr "\n" " ")
+  _MYECHO "Loaded SSH keys" && _OK ":${_LOADEDKEYS}"
+fi
+_MYECHO "SSH Agent" && _OK ":Loaded"
 }
-
-
 
 
 # set 'sshagent-kill' function to remove key, kill agents linked to our socket if more than one and remove socket
@@ -72,7 +111,7 @@ sshagent-kill () {
 # get pid from exported agent
 _SSHPID () { awk -F'=|;' '/SSH_AGENT_PID/ {print $2}' <$HOME/.ssh/.ssh-agent ; }
 _BLU "### [dont] Kill the ssh-agent !"
-_MYECHO -e "Find Agent pid"
+_MYECHO "Find Agent pid"
 if [ -z "$SSH_AGENT_PID" ]; then
   if [ -z "$(_SSHPID)" ]; then
     _KO ":NotFound"
@@ -86,12 +125,12 @@ else
   [ "$SSH_AGENT_PID" != "$(_SSHPID)" ] && export SSH_AGENT_PID=$(_SSHPID)
   if ps aux |grep -q $SSH_AGENT_PID; then _OK ":pid=$SSH_AGENT_PID"; else _KO ":NotRunning"; fi
 fi
-_MYECHO -e "Remove Key"
+_MYECHO "Remove Key"
 if ssh-add -D &>/dev/null; then _OK; else _KO ":NoKey"; fi
-_MYECHO -e "Kill Agent"
+_MYECHO "Kill Agent"
 if ssh-agent -k &>/dev/null; then _OK; else _KO; fi
 if [ -e ${SSH_AUTH_SOCK} ]; then
-  _MYECHO -e "Remove remaining Socket"
+  _MYECHO "Remove remaining Socket"
   shred -zvu $SSH_AUTH_SOCK &>/dev/null && _OK || _KO
 fi
 if ps --user $(id -u) -F|grep -v grep|grep -q ssh-agent; then
@@ -112,7 +151,7 @@ for _PROCESSID in $(ps --no-header --user $(id -u) -F|grep -v grep|grep ssh-agen
   fi
 done
 # unset ssh vars
-rm -f /home/zeph/.ssh/.ssh-agent &>/dev/null
+rm -f $HOME/.ssh/.ssh-agent &>/dev/null
 unset $SSH_AUTH_SOCK
 unset $SSH_AGENT_PID
 fi
